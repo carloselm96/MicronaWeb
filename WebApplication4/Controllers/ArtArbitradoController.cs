f5c19b0d77e58f4153171d4f2907a089eb535862
CARLOS ERNESTO LOPEZ MIJANGOS <zS14001379@estudiantes.uv.mx>
2018-04-17 10:48:32 -0500

    Proyecto Inicial

---
+++ WebApplication4/Controllers/ArtArbitradoController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class ArtArbitradoController : Controller
+    {
+        // GET: ArtArbitrado
+        public ActionResult Index()
+        {
+            micronaEntities2 db = new micronaEntities2();
+            var art = db.artarbitrado.ToList();
+            return View(art);
+        }
+
+        // GET: ArtArbitrado/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ArtArbitrado/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ArtArbitrado/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ArtArbitrado/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ArtArbitrado/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ArtArbitrado/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ArtArbitrado/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ WebApplication4/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        [Authorize]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult Login(usuario u)
+        {
+            micronaEntities2 db = new micronaEntities2();
+            var user = db.usuario.Where(x => u.Usuario1 == x.Usuario1).FirstOrDefault();
+            if (user!=null)
+            {
+                if (user.Status == 1 && user.Contrasena== u.Contrasena)
+                {
+                    FormsAuthentication.SetAuthCookie(user.TipoUsuario+"", true);
+                    HttpContext.Session["idUser"] = user.idUsuario;
+                    HttpContext.Session["nombreUser"]= user.Nombre+ " " +user.Apelidos;
+                    HttpContext.Session["tipoUser"] = user.tipousuario1.nombre;
+                    return RedirectToAction("Index","Home");                    
+                }
+            }          
+            return RedirectToAction("Login", "Home");
+        }
+
+        [Authorize]
+        public ActionResult Logout()
+        {
+            HttpContext.Session["idUser"] = null; 
+            HttpContext.Session["nombreUser"] = null;
+            HttpContext.Session["tipoUser"] = null;
+            FormsAuthentication.SignOut();            
+            return RedirectToAction("Login", "Home");
+        }
+
+    }
+}
