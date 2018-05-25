fcd15653dd7cd65d2632bfd0545af1296556afc8
angel Moguel <anemce@hotmail.com>
2018-05-25 14:16:40 -0500

    Libros añadido, falta controlador

--- WebApplication4/Controllers/LibroController.cs
+++ WebApplication4/Controllers/LibroController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,8 +11,12 @@ namespace WebApplication4.Controllers
     public class LibroController : Controller
     {
         // GET: Libro
-        public ActionResult Index()
+        public ActionResult Index(string response)
         {
+            if (response != null)
+            {
+                ViewBag.response = int.Parse(response);
+            }
             microna2018Entities db = new microna2018Entities();
             var libros = db.libro.ToList();
             return View(libros);
@@ -26,30 +31,80 @@ namespace WebApplication4.Controllers
         // GET: Libro/Create
         public ActionResult Create()
         {
-
+            microna2018Entities db = new microna2018Entities();
+            ViewBag.tipolibro = db.tipolibro.ToList();
+            ViewBag.grupo = db.grupoacademico.ToList();
             return View();
         }
 
         // POST: Libro/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        [Authorize]
+        public ActionResult Create(libro lib, HttpPostedFileBase ffile, List<string> GrupoAcademico)
         {
+            archivo file = null;
             try
             {
-                // TODO: Add insert logic here
+                string dir = "~/Content/Archivos/Libros";
+                string fileName = "";
+                string path = "";
+                microna2018Entities db = new microna2018Entities();
+                if (!Directory.Exists(dir))
+                {
+                    DirectoryInfo di = Directory.CreateDirectory(Server.MapPath(dir));
+                }
+                if (ffile != null && ffile.ContentLength > 0)
+                {
+                    fileName = Path.GetFileName(ffile.FileName);
+                    path = Path.Combine(Server.MapPath(dir), DateTime.Now.ToString("yyyyMMddHHmmss") + "-" + fileName);
+                    ffile.SaveAs(path);
+                    file = new archivo();
+                    file.Nombre = fileName;
+                    file.url = path;
+                    db.archivo.Add(file);
+                    db.SaveChanges();
+                }
 
-                return RedirectToAction("Index");
+                if (file != null)
+                {
+                    lib.Archivo = file.idarchivo;
+                }
+                lib.Usuario = int.Parse(Request.Cookies["userInfo"]["id"]);
+                db.libro.Add(lib);
+                if (GrupoAcademico != null)
+                {
+                    foreach (var s in GrupoAcademico)
+                    {
+                        libro_grupo ag = new libro_grupo
+                        {
+                            id_libro = lib.idLibro,
+                            id_grupo = int.Parse(s)
+                        };
+                        db.libro_grupo.Add(ag);
+                    }
+                }
+                db.SaveChanges();
+                return RedirectToAction("Index", new { response = 1 });
             }
             catch
             {
-                return View();
+                return RedirectToAction("Index", new { response = 2 });
             }
         }
 
         // GET: Libro/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            microna2018Entities db = new microna2018Entities();
+            var a = db.libro.Where(x => x.idLibro == id).FirstOrDefault();
+            if (int.Parse(Request.Cookies["userInfo"]["id"]) != a.Usuario)
+            {
+                return RedirectToAction("Index");
+            }
+            a.libro_grupo = db.libro_grupo.Where(x => x.id_libro == id).ToList();
+            ViewBag.grupos = db.grupoacademico.ToList();
+            ViewBag.tipolibro = db.tipolibro.ToList();
+            return View(a);
         }
 
         // POST: Libro/Edit/5
